c87f9a09544ae6a016bd62339a1a69dd9d740225
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-04-12 14:35:49 -0300

    feat: enables experimental rust compiler

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
-// @ts-check
-
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import { defineConfig, fontProviders } from "astro/config"
 
 import cloudflare from "@astrojs/cloudflare"
 
-// https://astro.build/config
 export default defineConfig({
+  experimental: {
+    rustCompiler: true
+  },
+
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
 
