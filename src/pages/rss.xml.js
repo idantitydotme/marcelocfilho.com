b856270f1618df67abab3de29e6dccc4cfcee81b
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-04-25 23:48:16 -0300

    feat: refines rss generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,18 @@
-import { getCollection } from "astro:content"
 import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts"
 
 export async function GET(context) {
-  const posts = await getCollection("blog")
+  const blog = await getCollection("blog")
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => Object.assign(post.data, { link: `/blog/${post.id}/` }))
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.id}/`
+    }))
   })
 }
