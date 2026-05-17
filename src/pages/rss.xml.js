3bbbdfbacda8dafcf98c3b1752470c3bdf12783f
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-17 08:14:14 -0300

    fix: fixes deprecated import in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts"
+import { siteConfig } from "@/site.config"
 
 export async function GET(context) {
   const blog = await getCollection("blog")
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: siteConfig.name,
+    description: siteConfig.description,
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
