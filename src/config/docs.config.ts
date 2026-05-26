81a0d1221517cb2a5308cff29853b551ce163807
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-26 17:53:30 -0300

    fix: updates legal

--- src/config/docs.config.ts
+++ src/config/docs.config.ts
@@ -2,7 +2,40 @@ import { defineDocsConfig } from "@rimelight/ui/utils";
 
 export default defineDocsConfig({
   editLink: {
-    baseUrl: "https://github.com/idantitydotme/marcelocfilho.com/edit/main/src/content/docs/"
+    baseUrl: "https://github.com/rimelight/rimelight/edit/main/packages/playground/src/content/docs/"
   },
-  sidebar: []
+  sidebar: [
+    {
+      id: "overview",
+      label: "Overview",
+      href: "/{locale}/docs/",
+      icon: "i-lucide-book-open",
+      items: [
+        {
+          label: "Home",
+          href: "/{locale}/docs/"
+        },
+        {
+          label: "Getting Started",
+          autogenerate: {
+            directory: ""
+          }
+        }
+      ]
+    },
+    {
+      id: "tests",
+      label: "Tests",
+      href: "/{locale}/docs/tests/keyboard/",
+      icon: "i-lucide-terminal",
+      items: [
+        {
+          label: "Testing Suite",
+          autogenerate: {
+            directory: "tests/"
+          }
+        }
+      ]
+    }
+  ]
 });
