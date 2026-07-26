60c655cd252cf051ba5d19e0cc42f242bc46eb6c
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-07-26 13:01:01 -0300

    refactor: standardization fixes

---
+++ src/i18n/loader.ts
@@ -0,0 +1,5 @@
+import { createTranslationLoader } from "@rimelight/i18n"
+import en from "@/translations/en.json"
+import pt from "@/translations/pt.json"
+
+export default createTranslationLoader({ en, pt })
