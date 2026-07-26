25f08072d88af545adf054f59f829bdbc7ddb190
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-07-26 16:09:40 -0300

    refactor: standardization fixes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,5 @@
 import { Hono } from "hono"
 import authRoutes from "./routes/auth"
-import searchRoutes from "./routes/search"
-import changelogRoutes from "./routes/changelog"
-import assetsRoutes from "./routes/assets"
-import pagesRoutes from "./routes/pages"
-import versionRoutes from "./routes/versions"
 import emailPreviewRoutes from "./routes/email/preview"
 import constructionGuestRoutes from "./routes/construction-guest"
 
@@ -12,11 +7,6 @@ const api = new Hono()
 
 api.route("/auth", authRoutes)
 api.route("/construction-guest", constructionGuestRoutes)
-api.route("/search", searchRoutes)
-api.route("/github", changelogRoutes)
-api.route("/assets", assetsRoutes)
-api.route("/pages", pagesRoutes)
-api.route("/pages/versions", versionRoutes)
 api.route("/email/preview", emailPreviewRoutes)
 
 export default api
