a315dcca230c16597c9eae65178471a9ec96759f
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-07-29 17:42:30 -0300

    refactor: standardization fixes

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,27 +14,25 @@ const legal = defineCollection({
 
 const blog = defineCollection({
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  schema: ({ image }) =>
-    z.object({
-      type: z.enum(["other", "personal-stories", "project-updates"]),
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional()
-    })
+  schema: z.object({
+    type: z.enum(["other", "personal-stories", "project-updates"]),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional()
+  })
 })
 
 const projects = defineCollection({
   loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional()
-    })
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional()
+  })
 })
 
 export const collections = {
