87e73c90cf592e3bd14b511f17807e5b738d88a3
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-09-14 21:34:39 -0300

    refactor: unifies config

--- astro.config.ts
+++ astro.config.ts
@@ -8,25 +8,6 @@ export default defineConfig(
     cms: true,
     security: true,
     i18n: true,
-    ui: {
-      logos: {
-        logomark: {
-          color: "./src/assets/logos/logomark_color.svg",
-          white: "./src/assets/logos/logomark_white.svg",
-          black: "./src/assets/logos/logomark_black.svg"
-        },
-        logotype: {
-          color: "./src/assets/logos/logotype_color.svg",
-          white: "./src/assets/logos/logotype_color.svg",
-          black: "./src/assets/logos/logotype_black.svg"
-        }
-      },
-      shortcuts: {
-        categories: {
-          system: { label: "System" },
-          navigation: { label: "Navigation" }
-        }
-      }
-    }
+    ui: true
   })
 )
