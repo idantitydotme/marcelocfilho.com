07f1101510e131a0c9439ae3bfd765a49afde930
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-09-19 09:35:20 -0300

    refactor: migrates from astro actions to pure hono

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,11 @@
 import { Hono } from "hono"
 import authRoutes from "./routes/auth"
 import searchRoutes from "./routes/search"
-import emailPreviewRoutes from "./routes/email/preview"
 import cmsRoutes from "./routes/cms"
 
 const api = new Hono()
   .route("/auth", authRoutes)
   .route("/search", searchRoutes)
-  .route("/dev/email/preview", emailPreviewRoutes)
   .route("/cms", cmsRoutes)
 
 export type ApiType = typeof api
